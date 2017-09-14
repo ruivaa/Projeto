f806ac3e04dabb5de0960f21d2f91b67ae22437e
root <root@jessie.raw>
2017-09-14 16:45:35 +0000

    x

---
+++ Desafio01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash clear echo echo 'Você é menino ou menina?'
+echo 'Você gosta de linux?'
+echo
+echo 'Menino ou menina?: '
+read -s SEXUALIDADE
+echo 'E ai, gosta?: '
+read -s GOSTO
+if [ $SEXUALIDADE == 'menina' ] ; then
+echo 'Você é uma moça muito legal!'
+else 
+echo 'Você um dia vai gostar'
+Sif [ $ 
+
+if [ $SEXUALIDADE 
